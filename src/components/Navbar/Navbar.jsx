d80a5c74e1f70ff99ffce90afc273682ee2e3851
Emre <emrekurtt1655@gmail.com>
2021-06-29 01:18:37 +0200

    feat: navbar left par added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,23 @@
 import './navbar.scss'
+import {Person, Mail} from "@material-ui/icons"
 
 export default function navbar(){
     return(
-        <div className='navbar'>
-
+        <div className='navbar' >
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className='logo'>iuvenius.</a>
+                    <div className="itemContainer">
+                        <Person className='icon'/>
+                        <span>+49 0176 329 45 200</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className='icon'/>
+                        <span>emrekurtt16@gmail.com</span>
+                    </div>
+                </div>
+                <div className="right"></div>S
+            </div>
         </div>
     )
 }
