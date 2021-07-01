cb8c6f10121fff3eb56f80b00d1d1e9f5757bf30
Emre <emrekurtt1655@gmail.com>
2021-07-01 23:16:17 +0200

    portfolio entwurf completed

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,9 +1,87 @@
 import './portfolio.scss'
+import PortfolioList from '../PortfolioList/PortfolioList'
+import { useState, useEffect } from 'react'
+import {
+    featuredPortfolio,
+    frontendPortfolio,
+    mobilePortfolio,
+    backendPortfolio
+} from '../../data'
+
 
 export default function Portfolio(){
+    const [selected, setSelected] = useState('featured')
+    const [data, setData] = useState([])
+    const portfolioList = [
+        {
+            id:'featured',
+            title:"Featured"
+        },
+        {
+            id:'frontend',
+            title:"Front End Application"
+        },
+        {
+            id:'mobile',
+            title:"Mobile Application"
+        },
+        {
+            id:'backend',
+            title:"Backend Application"
+        },
+    ]
+
+    useEffect(()=> {
+        switch (selected) {
+            case "featured":
+                setData(featuredPortfolio)
+                break;
+            case "frontend":
+                setData(frontendPortfolio)
+                break;
+            case "mobile":
+                setData(mobilePortfolio)
+                break;
+            case "backend":
+                setData(backendPortfolio)
+                break;
+            default:
+                setData(featuredPortfolio)
+                break;
+        }
+    }, [selected])
+
     return(
         <div className='portfolio' id='portfolio'>
+            <h1>Portfolio</h1>
+            <ul>
+                {
+                    portfolioList.map((item)=>(
+                        <PortfolioList title={item.title} 
+                        active={selected === item.id}
+                        setSelected={setSelected}
+                        id ={item.id}/>
+                    ))
+                }
+            </ul>
+            <div className="container">
 
+                {
+                    data.map((d)=>(
+                        <div className="item">
+                        <img src={d.img} alt="foto" />
+                        <h3>{d.title}</h3>
+                    </div>
+                    ))
+                }
+
+            
+            
+            
+               
+                
+            </div>
+            
         </div>
     )
 }
