bbedfcb9fc16c157e5d39feba9bbccbd657e9d59
Emre <emrekurtt1655@gmail.com>
2021-07-02 22:23:39 +0200

    works left part added

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -3,6 +3,25 @@ import './works.scss'
 export default function Works(){
     return(
         <div className='works' id="works">
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img src="./assets/mobile.png" alt="" />
+                                </div>
+                                <h2>Title</h2>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto voluptas harum maiores. Architecto rem quod, sapiente deserunt adipisci illo est 
+                                </p>
+                                <span>Projects</span>
+                            </div>
+                        </div>
+                        <div className="right"></div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
